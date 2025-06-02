8f118a6bc8338825b991fe454669a5749c878d35
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-06-01 23:58:33 -0500

    betterStyles

--- roomclass-app/src/app/shared/sidebar/sidebar.component.ts
+++ roomclass-app/src/app/shared/sidebar/sidebar.component.ts
@@ -3,24 +3,29 @@ import { AuthService } from '../../core/auth/auth.service';
 import { RouterModule } from '@angular/router';
 import { User } from '../../core/models/user';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   user: User | null = null;
+  isCoordinador = false;
+
   constructor(private authService: AuthService, private router: Router) {}
   // This method is called when the component is initialized, it obtains the user credentials
   ngOnInit() {
       this.authService.getProfile().subscribe({
         next: (user) => {
           this.user = user;
+          this.isCoordinador = (user.rol || '').trim().toUpperCase() === 'COORDINADOR';
         }
       });
+      console.log('User profile loaded:', this.user);
     }
   logout() {
     this.authService.logout();    
