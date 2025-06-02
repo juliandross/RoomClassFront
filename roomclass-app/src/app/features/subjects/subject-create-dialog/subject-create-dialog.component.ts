dfbbd184eadd519257a1b05bbbf09e73dc10876f
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-06-02 01:42:54 -0500

    dialog

---
+++ roomclass-app/src/app/features/subjects/subject-create-dialog/subject-create-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-create-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './subject-create-dialog.component.html',
+  styleUrl: './subject-create-dialog.component.css'
+})
+export class SubjectCreateDialogComponent {
+
+}
