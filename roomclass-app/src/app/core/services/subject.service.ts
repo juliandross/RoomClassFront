01ec4f582da6ed7eb1bc96b155b7855a2695a184
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-06-02 00:57:43 -0500

    generic-list-better

--- roomclass-app/src/app/core/services/subject.service.ts
+++ roomclass-app/src/app/core/services/subject.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Subject } from '../models/subject';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectService {
   private apiUrl = 'http://localhost:8001/AcademApi/subject/';
-  constructor(private httpClient:HttpClient) { }
-  getSubjects():Observable<Subject[]> {
-    return this.httpClient.get<Subject[]>(this.apiUrl);
-    //Simulation of an API response with fake data
-    /*const subjects: Subject[] = [
-      { id: 1, name: 'Matemáticas', description:'Se aprende a sumar', credits: 4, semester: 1 },
-      { id: 2, name: 'Física', description:'Se aprende a sumar', credits: 3, semester: 1 },
-      { id: 3, name: 'Programación', description:'Se aprende a sumar', credits: 5, semester: 2 }
-    ];
-    return of(subjects);*/
+  constructor(private httpClient: HttpClient, private authService: AuthService) {}
 
+  getSubjects(): Observable<Subject[]> {
+    const token = this.authService.getToken(); // Usa el método de AuthService
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    return this.httpClient.get<Subject[]>(this.apiUrl, { headers });
   }
 }
